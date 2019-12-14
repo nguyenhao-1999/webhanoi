0913b8ddcd987ee76304992b929772fb916f4e05
= <=>
2019-12-14 16:44:28 +0700

    chỉnh js

--- public/js/script.js
+++ public/js/script.js
@@ -9,17 +9,84 @@ $(document).ready(function(){
 	$.getScript("public/js/library.js");
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getEmail() {
+	var EmailRegister = $("#EmailRegister").val();
+	$('.loading').show();
+	$.ajax({
+		type: 'POST',
+		url: 'ajax/contect-email.php',
+		data: 'EmailRegister=' + EmailRegister,
+		dataType: 'json',
+		success: function (data) {
+			var content = data.Html;
+			var param = data.Params;
+			if (param != '') {
+				Swal.fire({
+					type: 'error',
+					html: param,
+					showCloseButton: true,
+					showCancelButton: true,
+					focusConfirm: false,
+					confirmButtonText: "OK",
+				})
+			}
+			if (content != '') {
+                //Swal.fire({
+                //    type: 'success',
+                //    html: content,
+                //})
+                location.href = "/dang-ky-thanh-cong";
+            }
+            $('.loading').hide();
+        },
+        error: function () { }
+    });
+}
+
+
+
+
 $(document).ready(function() {
 	$('#customemail').on('click',function(){
 		var input=$('#EmailRegister').val();
+		$('#form-err-customer').addClass("swal2-container");
 		$.ajax({
-			url: 'views/pages/contect-email.php',
+			url: 'ajax/contect-email.php',
 			type: 'POST',
 			dataType: 'json',
 			data: {input: input},
 		})
 		.done(function() {
 			console.log("success");
+
+			if(result.constructor === String){
+				result = JSON.parse(result);
+			}
+
+			if (result.success) {
+
+				$("body").html(result.msg);
+				$("#"+result.el).val();
+
+			}else{
+
+				$("body").html(result.msg);
+				$("#"+result.el).focus("#"+result.el);
+
+			}
 		})
 		.fail(function() {
 			console.log("error");
